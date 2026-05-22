cd25e760020a733a35c9e9aafb33756c841560a3
Esteban DAUCHY <estebandauchy@gmail.com>
2026-05-22 15:41:36 +0200

    Exercice 5 - Logique métier

--- src/main/java/org/example/DiceScore.java
+++ src/main/java/org/example/DiceScore.java
@@ -1,12 +1,20 @@
 package org.example;
 
-import jdk.jshell.spi.ExecutionControl;
-
 public class DiceScore {
 
     private InterfaceDiceMock de;
 
-    public int getScore() throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("Pas encore implémenté, le TDD oblige");
+    public int getScore() {
+        int scoreFirst = de.getRoll();
+        int scoreSecond = de.getRoll();
+
+        if (scoreFirst == scoreSecond) {
+            if (scoreFirst == 6) {
+                return 30;
+            }
+            return scoreFirst * 2 + 10;
+        } else {
+            return scoreFirst < scoreSecond ? scoreSecond : scoreFirst;
+        }
     }
 }
